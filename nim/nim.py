584b30526f85e54d35c0429ef943d02780a6f464
Uwe Hannen <uwe-fun@mdcc-fun.de>
2020-10-01 09:52:45 +0200

    Das NIM-Spiel in einer GUI-Version

--- nim/nim.py
+++ nim/nim.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
-import os, sys, time, random, copy
-import pygame as pg
-import button as bu
+#!/usr/bin/env python3
+import os, sys, time, random, copy, pygame as pg, button as bu
 
 PIKES = 3
 LIM_MIN, LIM_MAX = 1, 8
+FPS = 60
 # ---------------------------------------------
-DISPLAY_CAPTION = 'Das ganz grosse NIM-Spiel'
+DISPLAY_CAPTION = 'Das grosse NIM-Spiel'
 CTR_BUTTON_TXT = ("Ich habe fertig", "Weiter geht's", "Neues Spiel")
 # ---------------------------------------------
 PIC_WIDTH = 50
@@ -30,19 +29,15 @@ BG_COLOR = (47, 79, 79)
 BU_BO_COLOR = (0, 0, 139)
 BU_HO_COLOR = (0, 255, 255)
 BU_COLOR = (30, 144, 255)
-BU_TXT_COLOR = (255,250,250)
-# ---------------------------------------------
-STA_HIDDEN = 0
-STA_INACT = 1
-STA_ACTIV = 2
-STA_OPPONENT = 3
-STA_SELF = 4
-# ---------------------------------------------
-GA_INIT = 0
-GA_PLAY = 1
-GA_DEAL = 2
-# ---------------------------------------------
-FPS = 60
+BU_TXT_COLOR = (255, 250, 250)
+
+class Enum(object):
+  def __init__(self, tpList):
+    self.tpList = tpList
+  def __getattr__(self, name):
+    return self.tpList.index(name)
+STATUS = Enum(('HIDDEN', 'INACT', 'ACTIV', 'OPPONENT', 'SELF'))
+GAME = Enum(('INIT', 'PLAY', 'DEAL'))
 
 def xyPos(nr):
     x = ( nr % LIM_MAX ) * PIC_WIDTH + DISPLAY_DIST
@@ -52,7 +47,7 @@ def xyPos(nr):
 class Match_Area:
     def __init__(self, screen):
         self._screen = screen
-        self._globs = { 'game': GA_INIT, 'row': NO_SELECT, 'count': 0, 'wins': [0, 0] }
+        self._globs = { 'game': GAME.INIT, 'row': NO_SELECT, 'count': 0, 'wins': [0, 0] }
         self._pics = []
         self._matches = []
         self._los = []
@@ -60,7 +55,7 @@ class Match_Area:
         for x in range(1, 5):
             self._pics.append(pg.image.load(os.path.join('./res', "nim_{}_pse.png".format(x))))
         for x in range(PIKES * LIM_MAX):
-            self._matches.append({'stat': STA_HIDDEN, 'pos': xyPos(x)})
+            self._matches.append({'stat': STATUS.HIDDEN, 'pos': xyPos(x)})
         random.seed()
         self.newGame()  
     def draw(self):
@@ -69,49 +64,49 @@ class Match_Area:
             if k:
                 self._screen.blit(self._pics[k - 1].convert_alpha(),(self._matches[x]['pos']))    
     def newGame(self):
-        if self._globs['game'] != GA_INIT:
+        if self._globs['game'] != GAME.INIT:
             return
         pg.display.set_caption(DISPLAY_CAPTION)
         for x in range(PIKES * LIM_MAX):
-            self._matches[x]['stat'] = STA_HIDDEN
+            self._matches[x]['stat'] = STATUS.HIDDEN
         self._los = [ random.randint(LIM_MIN, LIM_MAX) for x in range(PIKES) ]
         self._actLos = copy.deepcopy(self._los)
         for i, v in enumerate(self._actLos):
             b = i * LIM_MAX
             for x in range(b, b + v):
-               self._matches[x]['stat'] = STA_ACTIV
-        self._globs['game'] = GA_PLAY
+               self._matches[x]['stat'] = STATUS.ACTIV
+        self._globs['game'] = GAME.PLAY
     def playerSelect(self, pos):
-        if self._globs['game'] != GA_PLAY:
+        if self._globs['game'] != GAME.PLAY:
             return
         col = (pos[0] - DISPLAY_DIST) // PIC_WIDTH 
         row = (pos[1] - DISPLAY_DIST) // PIC_HIGHT
         ind = LIM_MAX * row + col
         sta = self._matches[ind]['stat']
-        if  sta > STA_INACT:
+        if  sta > STATUS.INACT:
             if self._globs['row'] == NO_SELECT:
                self._globs['row'] = row
             if self._globs['row'] == row:   
-                if sta == STA_ACTIV: 
-                    self._matches[(self._globs['row'] * LIM_MAX + self._globs['count'])]['stat'] = STA_OPPONENT
+                if sta == STATUS.ACTIV: 
+                    self._matches[(self._globs['row'] * LIM_MAX + self._globs['count'])]['stat'] = STATUS.OPPONENT
                     self._globs['count'] += 1
-                elif sta == STA_OPPONENT:
+                elif sta == STATUS.OPPONENT:
                     self._globs['count'] -= 1
-                    self._matches[(self._globs['row'] * LIM_MAX + self._globs['count'])]['stat'] = STA_ACTIV
+                    self._matches[(self._globs['row'] * LIM_MAX + self._globs['count'])]['stat'] = STATUS.ACTIV
                     if not self._globs['count']:
                         self._globs['row'] = NO_SELECT
     def dealerSelect(self):
-        if self._globs['game'] != GA_PLAY or self._globs['row'] == NO_SELECT:
+        if self._globs['game'] != GAME.PLAY or self._globs['row'] == NO_SELECT:
             return
         self._actLos[self._globs['row']] -= self._globs['count']
         if sum(x for x in self._actLos) == 0:
             self._globs['wins'][0] += 1
             self._globs['row'] = NO_SELECT
             self._globs['count'] = 0
-            self._globs['game'] = GA_INIT
+            self._globs['game'] = GAME.INIT
             self.setCaption()
             return
-        self._globs['game'] = GA_DEAL    
+        self._globs['game'] = GAME.DEAL    
         selList = [ x for x in range(PIKES) ]
         random.shuffle(selList)
         korr = 0
@@ -138,27 +133,27 @@ class Match_Area:
         if self._globs['row'] == x:
             startpoint += self._globs['count'] 
         for x in range(startpoint, startpoint + korr):
-            self._matches[x]['stat'] = STA_SELF
+            self._matches[x]['stat'] = STATUS.SELF
         self._globs['row'] = NO_SELECT
         self._globs['count'] = 0
         if sum(x for x in self._actLos) == 0:
             self._globs['wins'][1] += 1
             self.setCaption()
     def result(self):
-        if self._globs['game'] != GA_DEAL:
+        if self._globs['game'] != GAME.DEAL:
             return
         for ind, val in enumerate(self._actLos):
             ul = ind * LIM_MAX
             ml = ul + val
             ol = ul + self._los[ind]
             for x in range(ul, ml):
-                self._matches[x]['stat'] = STA_ACTIV
+                self._matches[x]['stat'] = STATUS.ACTIV
             for x in range(ml, ol):
-                self._matches[x]['stat'] = STA_INACT
+                self._matches[x]['stat'] = STATUS.INACT
         if sum(x for x in self._actLos):
-            self._globs['game'] = GA_PLAY
+            self._globs['game'] = GAME.PLAY
         else:
-            self._globs['game'] = GA_INIT
+            self._globs['game'] = GAME.INIT
     def setCaption(self):
         pg.display.set_caption("{} : {}".format(self._globs['wins'][0], self._globs['wins'][1]))
 
