cda9934a7362e2285019d080050b5c6c9087b8c0
Uwe Hannen <uwe-fun@mdcc-fun.de>
2021-01-26 12:22:41 +0100

    Einfache Ausgabe über die serielle Schnittstelle

---
+++ arduino-snippet-01/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>            // Achtung: Arduino-IDE: Diese Zeile auskommentieren !!! 
+
+/******************************************************************************************
+ * Snippet : Serieller Output - Text und Zahlen gemischt                                  *
+ *                                                                                        *
+ * 26.01.2021                                                                             *
+ * Uwe Hannen                                                                             *
+ * ***************************************************************************************/
+
+#define INTERVALL 1000                                  
+#define CHAR_LENGTH 20                                  // Zeichenbreite
+
+void setup() {
+  Serial.begin(1200);
+}
+
+void loop() {
+  static unsigned int uiCounter = 0;
+  char szBuffer[CHAR_LENGTH];
+  const char *szMsg = "Ein Text";
+  if (millis() / INTERVALL > uiCounter) {               // delay - Ersatz
+    sprintf(szBuffer, "%-14s%6i", szMsg, uiCounter++);  // Puffer füllen 14 + 6 = 20 !!!
+    Serial.println(szBuffer);                           // und Ausgabe
+  }
+}
